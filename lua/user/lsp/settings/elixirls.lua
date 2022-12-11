70312e770cf377a1a035b8b6edf2447c50b7ec8b
nkpremices <premices.tuvere@gmail.com>
2022-12-11 13:15:24 +0300

    src(LSP): Fix elixir and pyright configs

--- lua/user/lsp/settings/elixirls.lua
+++ lua/user/lsp/settings/elixirls.lua
@@ -1,15 +1,10 @@
 return {
-  cmd = { "/Users/nkpremices/.local/share/nvim/mason/bin/language_server.sh" },
-  settings = {
-    elixirLS = {
-      -- I choose to disable dialyzer for personal reasons, but
-      -- I would suggest you also disable it unless you are well
-      -- aquainted with dialzyer and know how to use it.
-      dialyzerEnabled = false,
-      -- I also choose to turn off the auto dep fetching feature.
-      -- It often get's into a weird state that requires deleting
-      -- the .elixir_ls directory and restarting your editor.
-      fetchDeps = false
-    }
+  cmd = { "/Users/nkpremices/.local/share/nvim/mason/packages/elixir-ls/language_server.sh" },
+  flags = {
+    debounce_text_changes = 150,
+  },
+  elixirLS = {
+    dialyzerEnabled = false,
+    fetchDeps = false,
   }
 }
